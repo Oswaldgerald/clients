5407f0abd7febca98705987448931f375e7cc0d7
oswald <moswaldgerald@gmail.com>
2018-11-14 12:01:45 +0300

    dashboard

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Client
 from .forms import ClientForm
 from django.contrib.auth.decorators import login_required
+from rest_framework import viewsets
 
 
 def list_clients(request):
